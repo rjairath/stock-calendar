8b23a7c5f7890d611d8ef31237855b7a6b15477d
rishabh <rishabhjairath@gmail.com>
2022-01-18 01:11:30 +0530

    state changes on add and remove

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
-// const data = [
-//   { day: 1, price: 100 },
-//   { day: 2, price: 200 },
-// ];
-const data = {
-  1: { price: 100 },
-  2: { price: 200 },
-};
+const data = [
+  { date: "01/01/2022", price: 100 },
+  { date: "02/01/2022", price: 200 },
+];
+
 const MyCalendar = () => {
   const [date, setDate] = useState(new Date());
+  const [stockData, setStockData] = useState(data);
+  const [stockDataObj, setStockDataObj] = useState({});
+
+  useEffect(() => {
+    let obj = {};
+    stockData.forEach((stock) => {
+      obj[stock.date] = stock.price;
+    });
+    setStockDataObj(obj);
+  }, [stockData]);
 
   const onChange = (calDate) => {
     //TODO
@@ -19,18 +26,21 @@ const MyCalendar = () => {
   };
 
   const removeClicked = (date) => {
-    console.log(date);
+    let dateStr = date.toLocaleDateString();
+    setStockData(stockData.filter((item) => item.date != dateStr));
   };
 
   const addClicked = (date) => {
-    console.log(date);
+    let dateStr = date.toLocaleDateString();
+    let newObj = { date: dateStr, price: 1000 };
+    setStockData([...stockData, newObj]);
   };
 
   const setCalendar = (date) => {
-    let dateVal = date.getDate();
-    return data[dateVal] ? (
+    let dateVal = date.toLocaleDateString();
+    return stockDataObj[dateVal] ? (
       <div className="calendar-date">
-        <span className="price">{data[dateVal].price}</span>
+        <span className="price">{stockDataObj[dateVal]}</span>
         <span className="close" onClick={() => removeClicked(date)}>
           X
         </span>
@@ -44,11 +54,13 @@ const MyCalendar = () => {
 
   return (
     <div>
+      (
       <Calendar
         onChange={onChange}
         value={date}
         tileContent={({ date }) => setCalendar(date)}
       />
+      )
     </div>
   );
 };
